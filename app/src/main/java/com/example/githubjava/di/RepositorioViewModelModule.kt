4a861eba268b9ba14210f7695e8e2c8cfc0c6634
wesley oliveira <wlwwesley9@gmail.com>
2023-07-14 19:54:47 -0300

    Otimizando a forma como o inject esta sendo realizado no repositorioActivty

--- app/src/main/java/com/example/githubjava/di/RepositorioViewModelModule.kt
+++ app/src/main/java/com/example/githubjava/di/RepositorioViewModelModule.kt
@@ -1,12 +1,23 @@
 package com.example.githubjava.di
 
+import com.example.githubjava.data.repository.SearchPullRequestUseCaseImpl
+import com.example.githubjava.domain.pullRequest.SearchPullRequestUseCase
 import com.example.githubjava.presentation.repositorio.RepositorioActivity
 import com.example.githubjava.presentation.repositorio.viewmodel.RepositorioViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
 val repositorioViewModelModule = module {
 
+    factory<SearchPullRequestUseCase>  {
+        SearchPullRequestUseCaseImpl()
+    }
+
     viewModel { RepositorioViewModel(get()) }
 
 }
+
+internal val loadingRepositorioViewModelModule by lazy { loadKoinModules(repositorioViewModelModule) }
+
+internal fun injectRepositorioViewModelModule() = loadingRepositorioViewModelModule
