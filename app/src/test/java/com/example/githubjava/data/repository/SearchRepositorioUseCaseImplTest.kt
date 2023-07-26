9ddeed111fd894098d70511a7147eecbd12fe258
wesley oliveira <wlwwesley9@gmail.com>
2023-07-25 21:56:44 -0300

    problemas para mockar a string page

--- app/src/test/java/com/example/githubjava/data/repository/SearchRepositorioUseCaseImplTest.kt
+++ app/src/test/java/com/example/githubjava/data/repository/SearchRepositorioUseCaseImplTest.kt
@@ -3,7 +3,9 @@ package com.example.githubjava.data.repository
 import com.example.githubjava.data.repository.SearchRepositorioUseCaseImpl
 import com.example.githubjava.domain.models.OwnerModel
 import com.example.githubjava.domain.models.Repositorio
+import io.mockk.MockK
 import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -12,22 +14,27 @@ import retrofit2.Response
 
 class SearchRepositorioUseCaseImplTest {
 
+    @io.mockk.impl.annotations.MockK
     private lateinit var searchRepositorioUseCaseImpl: SearchRepositorioUseCaseImpl
+    @io.mockk.impl.annotations.MockK
+    private lateinit var page: String
 
     @Before
     fun setup(){
         searchRepositorioUseCaseImpl = SearchRepositorioUseCaseImpl()
+        page = mockk("1")
     }
 
     @Test
     fun `should_return_repositorios_when_request_return_success`() = runTest{
 
-        val listRepositorios = mutableListOf<Repositorio>( Repositorio("","","","","","", owner = OwnerModel("","")))
+        val listRepositorios = mutableListOf<Repositorio>( mockk<Repositorio>())
 
         // Arrange
-        coEvery { searchRepositorioUseCaseImpl.fetchCurrencies("1") }
-        
+        coEvery { searchRepositorioUseCaseImpl.fetchCurrencies(page) } returns listRepositorios
+
         // Act
+        searchRepositorioUseCaseImpl.fetchCurrencies(page)
 
         // Assert
 
