e54509dd14977f43781ddea0f8553e2b0d507a72
wesley oliveira <wlwwesley9@gmail.com>
2023-08-01 20:15:48 -0300

    teste caso o nomeRepo venha vazio

--- app/src/test/java/com/example/githubjava/data/repository/SearchRepositorioUseCaseImplTest.kt
+++ app/src/test/java/com/example/githubjava/data/repository/SearchRepositorioUseCaseImplTest.kt
@@ -1,5 +1,6 @@
 package com.example.githubjava.data.repository
 
+import androidx.lifecycle.viewmodel.savedstate.R
 import com.example.githubjava.data.repository.SearchRepositorioUseCaseImpl
 import com.example.githubjava.domain.models.OwnerModel
 import com.example.githubjava.domain.models.Repositorio
@@ -7,10 +8,14 @@ import io.mockk.MockK
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
-import kotlin.test.assertNotNull
+import java.util.Optional.empty
+import java.util.function.Predicate.not
+import kotlin.test.*
 
 
 class SearchRepositorioUseCaseImplTest {
@@ -26,15 +31,32 @@ class SearchRepositorioUseCaseImplTest {
     @Test
     fun `should_return_repositorios_when_request_return_success`() = runTest{
 
-        val listRepositorios = mutableListOf<Repositorio>( mockk<Repositorio>())
+        val listRepositorios = mutableListOf<Repositorio>( Repositorio("nomeTeste","descTeste","autorTeste","fotoTeste","10","10",OwnerModel("loginTeste","avatarTeste")))
 
         // Arrange
         coEvery { searchRepositorioUseCaseImpl.fetchCurrencies("1") } returns listRepositorios
 
         // Act
-       val result = searchRepositorioUseCaseImpl.fetchCurrencies("1")
+       val result : MutableList<Repositorio> = searchRepositorioUseCaseImpl.fetchCurrencies("1")
 
         // Assert
-        assertNotNull(result)
+        assertNotNull(result[0].nomeAutor)
+    }
+
+    @Test
+    fun `should_return_repositorios_when_request_return_nome_repo_empty`() = runTest{
+
+        val listRepositorios = mutableListOf<Repositorio>( Repositorio("","descTeste","autorTeste","fotoTeste","10","10",OwnerModel("loginTeste","avatarTeste")))
+
+        // Arrange
+        coEvery { searchRepositorioUseCaseImpl.fetchCurrencies("1") } returns listRepositorios
+
+        // Act
+        val result : MutableList<Repositorio> = searchRepositorioUseCaseImpl.fetchCurrencies("1")
+
+        // Assert
+        Assert.assertEquals(result[0].nomeRepositorio, "")
+
     }
 }
+
